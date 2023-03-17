3007b35fabb3fb8e05da91b5ef49338d105b47f3
Engin <tercumedunyam@gmail.com>
2023-03-17 14:55:30 +0300

    trying to push the code

--- src/test/java/first_git_package/MySecondClass.java
+++ src/test/java/first_git_package/MySecondClass.java
@@ -4,7 +4,9 @@ public class MySecondClass {
     public static void main(String[] args) {
 
         System.out.println("This is my second class");
-        System.out.println("UPDATING EXISTING CODE");  // bU KOD OTOMATİK OLARAK github'a gitmez. gitmesi için 3 kodu yaz
-        // malıyız.
+        System.out.println("UPDATING EXISTING CODE"); //KOD OTOMATİK OLARAK github'a gitmez. gitmesi için 3 kodu yazmalıyız.
+        // * git add .
+        // * git commit -m "added my second java class"
+        // * git push
     }
 }
